7c60a846dc732c3ad904edb03a8033f76e18fdff
Meet Mistry <meetm2003@gmail.com>
2024-08-08 23:54:27 +0530

    changes

--- animalwelfare/lib/main.dart
+++ animalwelfare/lib/main.dart
@@ -1,11 +1,34 @@
+import 'package:animalwelfare/firebase_options.dart';
 import 'package:animalwelfare/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() async {
+  // Ensure that Flutter bindings are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    // Initialize Firebase
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print('Firebase initialized successfully');
+  } catch (e) {
+    print("Firebase initialization error: $e");
+  }
+  
+  // Run the app
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
-    ),
-  );
+      home: SplashScreen(),
+    );
+  }
 }
